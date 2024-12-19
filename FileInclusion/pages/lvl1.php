996a48c5009b5562aa400c8ba3fcd6242cfbf634
BotAvs <137691516+BotAvs@users.noreply.github.com>
2024-12-19 16:05:06 +0100

    Update vulnerabilities lvl1.php

--- FileInclusion/pages/lvl1.php
+++ FileInclusion/pages/lvl1.php
@@ -13,17 +13,25 @@
       
       <div align="center"><b><h3>This is Level 1</h3></b></div>
       <div align="center">
-      <a href=lvl1.php?file=1.php><button>Button</button></a>
-      <a href=lvl1.php?file=2.php><button>The Other Button!</button></a>
+      <a href="lvl1.php?file=1.php"><button>Button</button></a>
+      <a href="lvl1.php?file=2.php"><button>The Other Button!</button></a>
       </div>
       
       <?php
         echo "</br></br>";
         
-        if (isset( $_GET[ 'file' ]))        
-        {
-          @include($_GET[ 'file' ]);
-          echo"<div align='center'><b><h5>".$_GET[ 'file' ]."</h5></b></div> ";       
+        if (isset($_GET['file'])) {
+          $file = basename($_GET['file']);  // Obtiene el nombre del archivo sin rutas
+          
+          // Lista blanca de archivos permitidos
+          $allowed_files = ['1.php', '2.php'];
+          
+          if (in_array($file, $allowed_files)) {
+            include($file);
+            echo "<div align='center'><b><h5>" . htmlspecialchars($file) . "</h5></b></div>";       
+          } else {
+            echo "<div align='center'><b><h5>Invalid file!</h5></b></div>";
+          }
         }
       ?>
    </body>
