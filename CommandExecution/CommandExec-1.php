78560938157f7a1eb2c6f380d84061c86aa8ffdc
BotAvs <137691516+BotAvs@users.noreply.github.com>
2024-12-19 15:59:51 +0100

    Update vulnerabilities CommandExec-1.php

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -10,24 +10,28 @@
     </div>
     <div style="background-color:#c9c9c9;padding:20px;">
       <h1 align="center">Login as Admin</h1>
-    <form align="center" action="CommandExec-1.php" method="$_GET">
-      <label align="center">Username:</label><br>
-      <input align="center" type="text" name="username" value="Admin"><br>
-      <label>Password:</label><br>
-      <input align="center" type="password" name="password" value=""><br>
-    <input align="center" type="submit" value="Submit">
-
-    </form>
-  </div>
-  <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
+      <form align="center" action="CommandExec-1.php" method="POST">
+        <label align="center">Username:</label><br>
+        <input align="center" type="text" name="username" value="Admin"><br>
+        <label>Password:</label><br>
+        <input align="center" type="password" name="password" value=""><br>
+        <input align="center" type="submit" value="Submit">
+      </form>
+    </div>
+    <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
     <?php
-    if(isset($_GET["username"])){
-      echo shell_exec($_GET["username"]);
-      if($_GET["username"] == "Admin" && $_GET["password"] == "ufoundmypassword")
-        echo "WELLDONE";
-    }
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
 
+        if ($username == "Admin" && $password == "ufoundmypassword") {
+          echo "WELLDONE";
+        } else {
+          echo "Invalid credentials!";
+        }
+      }
     ?>
-  </div>
+    </div>
   </body>
 </html>
+
