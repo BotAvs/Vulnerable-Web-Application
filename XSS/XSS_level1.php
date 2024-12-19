303f8351bf84aa256c102b205d42cd26610c4803
BotAvs <137691516+BotAvs@users.noreply.github.com>
2024-12-19 16:51:36 +0100

    Update test XSS_level1.php

--- XSS/XSS_level1.php
+++ XSS/XSS_level1.php
@@ -2,7 +2,7 @@
 <html>
 <head>
    <title>XSS 1</title>
-<link rel="shortcut icon" href="../Resources/hmbct.png" />
+   <link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
 	
@@ -10,15 +10,21 @@
       <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
       <button type="button" name="mainButton" onclick="location.href='xssmainpage.html';">Main Page</button>
     </div>
-<div align="center">
-   <form method="GET" action="" name="form">
-   <p>Your name:<input type="text" name="username"></p>
-   <input type="submit" name="submit" value="Submit">
-</form>
-	</div>
-<?php
-if(isset($_GET["username"]))
+    
+    <div align="center">
+       <form method="GET" action="" name="form">
+          <p>Your name: <input type="text" name="username" required></p>
+          <input type="submit" name="submit" value="Submit">
+       </form>
+    </div>
 
-	echo("Your name is ".$_GET["username"])?>
+<?php
+if (isset($_GET["username"])) {
+
+    $username = htmlspecialchars($_GET["username"], ENT_QUOTES, 'UTF-8');
+    echo("Your name is " . $username);
+}
+?>
+
 </body>
 </html>
